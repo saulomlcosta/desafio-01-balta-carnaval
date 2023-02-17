427f3e2847f9b610b279a414e195cf5809c92d61
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2023-02-17 15:49:28 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+leituraTexto();
+Console.ReadLine();
+
+void leituraTexto()
+{
+    Console.WriteLine("Digite um texto:");
+    string? textoDigitado = Console.ReadLine();
+    Console.WriteLine("Esse texto foi digitado no console:");
+    Console.WriteLine(textoDigitado);
+
+    string textoSemEspacos = Regex.Replace(textoDigitado, @"\s", "");
+
+    if (textoSemEspacos.Any())
+    {
+        List<string> quantidadePalavras = textoDigitado.Split(' ').ToList();
+        Console.WriteLine($"> {textoSemEspacos.Length} caracteres, {quantidadePalavras.Count()} palavras.");
+    }
+    else
+    {
+        Console.WriteLine("Digite ao menos algo ;)");
+        leituraTexto();
+    }
+}
